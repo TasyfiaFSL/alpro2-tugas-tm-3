63474c6918c4a7c3396fabcc420e160872ac61b1
TasyfiaFSL <tasyfiafarhahlubis@gmail.com>
2025-09-02 21:02:05 +0700

    Menambahkan program dasar Python (main.py) untuk pengujian GitHub

---
+++ main.py
@@ -0,0 +1,10 @@
+# Program sederhana untuk uji coba repository GitHub
+
+def main():
+    print("Halo, ini adalah program pertama di repository Alpro 2.")
+    print("Program ini dibuat untuk mencoba fitur GitHub.")
+    nama = input("Masukkan nama Anda: ")
+    print(f"Selamat datang, {nama}! Semoga sukses belajar Git & GitHub.")
+
+if __name__ == "__main__":
+    main()
